89852e608fa450ef6f87a92e62d76ed6d1e1ec4b
fahmanfaisal <fahmanchy@gmail.com>
2021-10-04 01:15:33 +0600

    timer funtionalities done

---
+++ script.js
@@ -0,0 +1,86 @@
+const btn= document.querySelector(".mobile-menu-btn");
+        const sidebar= document.querySelector(".sidebar");
+
+        btn.addEventListener("click", () => {
+            sidebar.classList.toggle("-translate-x-full");   
+        });
+
+
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var stop = document.getElementById('stop');
+
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
+
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
+
+//ref time variable
+
+var startTimer;
+
+start.addEventListener('click',function() {
+    if (startTimer === undefined) {
+        startTimer = setInterval(timer, 1000)
+    }else{
+        alert('Timer is already running');
+    }
+})
+
+reset.addEventListener('click', function () {
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click',function () {
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+
+
+//start timer function
+function timer() {
+    //work time countdown
+    if (ws.innerText != 0) {
+        ws.innerText--;
+    }else if (wm.innerText != 0 && ws.innerText == 0) {
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //break time countdown 
+    if (wm.innerText == 0 && ws.innerText == 0) {
+        if (bs.innerText != 0) {
+            bs.innerText--;
+        }else if (bm.innerText != 0 && bs.innerText == 0) {
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+
+    //counter set up
+    if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText ==0) {
+        wm.innerText = 25;
+        ws.innerText = "00";
+
+        bm.innerText = 5;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
+}
+
+//stop timer function
+function stopInterval() {
+    clearInterval(startTimer);
+}
